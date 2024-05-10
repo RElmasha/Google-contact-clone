eae364c7213650e86a242a6d68f34838f3db9a46
Rachel Mashaury <rachelmashaury1@gmail.com>
2024-05-10 02:42:47 +0200

    javaScript a part resolved

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,17 +5,39 @@
 //     document.getElementById("#addContact").style.display = "inline-block";
 // }
 
+const buttonBerger = document.getElementById("buttonBerger")
+buttonBerger.addEventListener("click", function () {
+  document.getElementById("sidebar").style.display = "none";
+  document.getElementById("contactList").style.marginLeft = "0%";
+           document.getElementById("contactList").style.display = "flex";
+           document.getElementById("contactList").style.width ="100%";
+           document.getElementById("container").style.margin = "0%"
+           document.getElementById("container").style.padding = "0%"
+       })  
+
+       const closeAddContact = document.getElementById("buttonBerger")
+       closeAddContact.addEventListener("click", function () {
+  document.getElementById("sidebar").style.display = "none";
+  document.getElementById("contactList").style.display = "none";
+  document.getElementById("addContact").style.marginLeft = "0%";
+  document.getElementById("addContact").style.display = "block";
+  document.getElementById("addContact").style.width ="100%";
+                  })  
+
 // function openBurger() {
 //     document.getElementById("main").style.marginLeft = "25%";
 //     document.getElementById("sidebar").style.width = "25%";
 //     document.getElementById("sidebar").style.display = "block";
 //     document.getElementById("openNav").style.display = 'none';
 //   }
-  
+
+
+
+
   const AddContacts = document.getElementById("add")
   AddContacts.addEventListener("click", function () {
        document.getElementById("contactList").style.display = "none";
-    document.getElementById("addContact").style.display = "flex";
+    document.getElementById("addContact").style.display = "block";
        })
   
    const showContactsList = document.getElementById("showContactList")
