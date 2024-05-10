5bb8626060c6cc68d1da70a653130ca40401de71
Rachel Mashaury <rachelmashaury1@gmail.com>
2024-05-10 10:05:36 +0100

    fixed a problem

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,25 +1,27 @@
-
-
-// function openBurger()
-
 const buttonBerger = document.getElementById("buttonBerger")
-buttonBerger.addEventListener("click", function () {
+
+function toggleSideBar() {
    const sidebar = document.getElementById("sidebar")
    if (sidebar.style.display === "none") {
       sidebar.style.display = "block";
-      document.getElementById("contactList").style.marginLeft = "20%";
-      document.getElementById("contactList").style.display = "flex";
-      document.getElementById("contactList").style.width = "80%";
+      document.getElementById("content").style.display = "block";
+
+      // document.getElementById("contactList").style.marginLeft = "20%";
+      // document.getElementById("contactList").style.display = "flex";
+      // document.getElementById("contactList").style.width = "80%";
    } else {
 
       sidebar.style.display = "none";
-      document.getElementById("contactList").style.marginLeft = "0%";
-      document.getElementById("contactList").style.display = "flex";
-      document.getElementById("contactList").style.width = "100%";
-      document.getElementById("container").style.margin = "0%"
-      document.getElementById("container").style.padding = "0%"
+      // document.getElementById("contactList").style.marginLeft = "0%";
+      // document.getElementById("contactList").style.display = "flex";
+      // document.getElementById("contactList").style.width = "100%";
+      // document.getElementById("container").style.margin = "0%"
+      // document.getElementById("container").style.padding = "0%"
    }
-})
+}
+
+
+buttonBerger.addEventListener("click", toggleSideBar)
 
 //        const AddContact = document.getElementById("buttonBerger")
 //        AddContact.addEventListener("click", function () {
