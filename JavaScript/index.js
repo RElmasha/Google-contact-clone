941aaac5fd7d96c60f28af9067c9b21ffaec937c
Rachel Mashaury <rachelmashaury1@gmail.com>
2024-05-10 12:28:15 +0100

    refactoring css

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,45 +5,14 @@ function toggleSideBar() {
    if (sidebar.style.display === "none") {
       sidebar.style.display = "block";
       document.getElementsByTagName("main").style.display = "inline";
-
-      // document.getElementById("contactList").style.marginLeft = "20%";
-      // document.getElementById("contactList").style.display = "flex";
-      // document.getElementById("contactList").style.width = "80%";
    } else {
 
       sidebar.style.display = "none";
-      // document.getElementById("contactList").style.marginLeft = "0%";
-      // document.getElementById("contactList").style.display = "flex";
-      // document.getElementById("contactList").style.width = "100%";
-      // document.getElementById("container").style.margin = "0%"
-      // document.getElementById("container").style.padding = "0%"
    }
 }
 
 
 buttonBerger.addEventListener("click", toggleSideBar)
-
-//        const AddContact = document.getElementById("buttonBerger")
-//        AddContact.addEventListener("click", function () {
-//   document.getElementById("sidebar").style.display = "none";
-//   document.getElementById("contactList").style.display = "none";
-//   document.getElementById("addContact").style.marginLeft = "0%";
-//   document.getElementById("addContact").style.display = "block";
-//   document.getElementById("addContact").style.width ="100%";
-//                   })  
-
-// End function 
-
-
-
-
-
-
-// function openSideBars()
-
-// End function 
-
-
 
 const AddContacts = document.getElementById("add")
 AddContacts.addEventListener("click", function () {
